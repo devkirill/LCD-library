02e8c3ccb2f197ca2b70543d7258f7b0b319027d
devkirill <deviatkin-kirill@ya.ru>
2014-10-21 13:32:39 +0600

    Добавил код для функции writeData

--- LCD/Nokia5110.cpp
+++ LCD/Nokia5110.cpp
@@ -20,9 +20,24 @@ TNokia5110::TNokia5110(
 
 };
 
+void TNokia5110::writeData(byte dc, byte data)
+{
+	if (dc == HIGH && invertColor)
+		data = ~data;
+	digitalWrite(PIN_DC, dc);
+	digitalWrite(PIN_SCE, LOW);
+	shiftOut(PIN_SDIN, PIN_SCLK, MSBFIRST, data);
+	digitalWrite(PIN_SCE, HIGH);
+}
+
+bool TNokia5110::paint(uint8_t x, uint8_t y)
+{
+
+}
+
 void TNokia5110::repaint()
 {
-	
+
 }
 
 void TNokia5110::begin()
@@ -36,11 +51,11 @@ void TNokia5110::begin()
 	digitalWrite(LED_, HIGH);
 	digitalWrite(RESET_, LOW);
 	digitalWrite(RESET_, HIGH);
-	writeData(LCD_C, 0x21);  // LCD Extended Commands.
-	writeData(LCD_C, 0xB1);  // Set LCD Vop (Contrast). 
-	writeData(LCD_C, 0x04);  // Set Temp coefficent. //0x04
-	writeData(LCD_C, 0x14);  // LCD bias mode 1:48. //0x13
-	writeData(LCD_C, 0x0C);  // LCD in normal mode.
-	writeData(LCD_C, 0x20);
-	writeData(LCD_C, 0x0C);
+	writeData(LOW, 0x21);  // LCD Extended Commands.
+	writeData(LOW, 0xB1);  // Set LCD Vop (Contrast). 
+	writeData(LOW, 0x04);  // Set Temp coefficent. //0x04
+	writeData(LOW, 0x14);  // LCD bias mode 1:48. //0x13
+	writeData(LOW, 0x0C);  // LCD in normal mode.
+	writeData(LOW, 0x20);
+	writeData(LOW, 0x0C);
 }
